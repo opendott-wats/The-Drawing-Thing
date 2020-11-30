60fd2e6147e9992fb9b3b9c047003592f596bf4e
jens alexander ewald <jens@lea.io>
2020-11-30 19:25:09 +0100

    Initial Commit

---
+++ haptic memories/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  haptic memories
+//
+//  Created by Jens Alexander Ewald on 30/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ haptic memories/haptic_memoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  haptic_memoriesApp.swift
+//  haptic memories
+//
+//  Created by Jens Alexander Ewald on 30/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct haptic_memoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
