0f89ff71c3c6993d81febc3d2ea5be687b90998e
jens alexander ewald <jens@lea.io>
2020-12-17 21:14:38 +0100

    Hide status bar

--- haptic memories/haptic_memoriesApp.swift
+++ haptic memories/haptic_memoriesApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct haptic_memoriesApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().statusBar(hidden: true)
         }
     }
 }
