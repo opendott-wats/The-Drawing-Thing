edd97febcef169559350a655824ab3189f376525
jens alexander ewald <jens@lea.io>
2021-11-30 23:25:14 +0100

    Adding code header with license and copyright info

--- haptic memories/ColourCamera.swift
+++ haptic memories/ColourCamera.swift
@@ -6,6 +6,27 @@
 //
 //  Created by jens ewald on 14/10/2021.
 //
+//  Copyright (C) 2021  jens alexander ewald <jens@poetic.systems>
+//  
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//  
+//  ------
+//  
+//  This project has received funding from the European Union’s Horizon 2020
+//  research and innovation programme under the Marie Skłodowska-Curie grant
+//  agreement No. 813508.
+// 
 
 import SwiftUI
 import UIKit
